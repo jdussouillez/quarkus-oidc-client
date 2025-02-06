68262c5809f1536d407cf7c9f420ce1a998fd0ad
Junior DUSSOUILLEZ <junior.dussouillez@hifi-filter.com>
2025-02-06 13:48:45 +0100

    feat: add code to reproduce the bug

--- testapp-common/src/main/java/com/github/jdussouillez/testapp/oidc/OidcTokenProvider.java
+++ testapp-common/src/main/java/com/github/jdussouillez/testapp/oidc/OidcTokenProvider.java
@@ -1,12 +1,17 @@
 package com.github.jdussouillez.testapp.oidc;
 
+import com.github.jdussouillez.testapp.Loggers;
 import io.quarkus.oidc.client.OidcClient;
 import io.quarkus.oidc.client.OidcClients;
+import io.quarkus.oidc.client.Tokens;
+import io.smallrye.mutiny.Uni;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -24,6 +29,8 @@ public class OidcTokenProvider {
 
     protected Map<String, OidcClient> clients;
 
+    protected final Map<String, Tokens> tokens = new ConcurrentHashMap<>(OIDC_IDS.size(), 1f);
+
     @PostConstruct
     protected void init() {
         clients = OIDC_IDS
@@ -31,5 +38,21 @@ public class OidcTokenProvider {
             .collect(Collectors.toUnmodifiableMap(Function.identity(), oidcClients::getClient));
     }
 
-    // ...
+    public Uni<String> getFooToken() {
+        return updateToken(FOO_OIDC_ID)
+            .map(Tokens::getAccessToken);
+    }
+
+    private Uni<Tokens> updateToken(final String clientId) {
+        var client = clients.get(clientId);
+        if (client == null) {
+            return Uni.createFrom().failure(() -> new NoSuchElementException("Client not found: " + clientId));
+        }
+        var token = tokens.get(clientId);
+        if (token == null || token.isAccessTokenExpired()) {
+            return client.getTokens()
+                .invoke(t -> tokens.put(clientId, t));
+        }
+        return Uni.createFrom().item(token);
+    }
 }
--- testapp-common/src/main/java/com/github/jdussouillez/testapp/service/TestService.java
+++ testapp-common/src/main/java/com/github/jdussouillez/testapp/service/TestService.java
@@ -1,6 +1,8 @@
 package com.github.jdussouillez.testapp.service;
 
+import com.github.jdussouillez.testapp.Loggers;
 import com.github.jdussouillez.testapp.oidc.OidcTokenProvider;
+import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -9,4 +11,13 @@ public class TestService {
 
     @Inject
     protected OidcTokenProvider oidcTokenProvider;
+
+    public Multi<String> fetch() {
+        return oidcTokenProvider.getFooToken()
+            .onItem()
+            .transformToMulti(token -> {
+                Loggers.MAIN.info("Token: {}", token);
+                return Multi.createFrom().items("foo", "bar", "baz");
+            });
+    }
 }
