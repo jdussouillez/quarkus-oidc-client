f02a1bc86838492969dfd1b928212a67e1545a4c
Junior DUSSOUILLEZ <junior.dussouillez@hifi-filter.com>
2025-02-06 13:22:07 +0100

    chore: remove unused code

--- testapp-common/src/main/java/com/github/jdussouillez/testapp/oidc/OidcTokenProvider.java
+++ testapp-common/src/main/java/com/github/jdussouillez/testapp/oidc/OidcTokenProvider.java
@@ -1,17 +1,12 @@
 package com.github.jdussouillez.testapp.oidc;
 
-import com.github.jdussouillez.testapp.Loggers;
 import io.quarkus.oidc.client.OidcClient;
 import io.quarkus.oidc.client.OidcClients;
-import io.quarkus.oidc.client.Tokens;
-import io.smallrye.mutiny.Uni;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -29,13 +24,6 @@ public class OidcTokenProvider {
 
     protected Map<String, OidcClient> clients;
 
-    protected volatile Map<String, Tokens> tokens = new ConcurrentHashMap<>(OIDC_IDS.size(), 1f);
-
-    public Uni<String> getFooToken() {
-        return updateToken(FOO_OIDC_ID)
-            .map(Tokens::getAccessToken);
-    }
-
     @PostConstruct
     protected void init() {
         clients = OIDC_IDS
@@ -43,26 +31,5 @@ public class OidcTokenProvider {
             .collect(Collectors.toUnmodifiableMap(Function.identity(), oidcClients::getClient));
     }
 
-    private Uni<Tokens> updateToken(final String clientId) {
-        var client = clients.get(clientId);
-        if (client == null) {
-            return Uni.createFrom().failure(() -> new NoSuchElementException("Client not found: " + clientId));
-        }
-        var token = tokens.get(clientId);
-        if (token == null || token.isAccessTokenExpired()) {
-            var refresh = token != null && token.isAccessTokenExpired();
-            // "tokens.getRefreshToken()" always returns null (expected behavior), so we need to
-            // create another token if it expired.
-            return client.getTokens()
-                .invoke(t -> {
-                    tokens.put(clientId, t);
-                    Loggers.MAIN.trace(
-                        "Azure AD token {} successfully for client '{}'",
-                        () -> refresh ? "refreshed" : "set",
-                        clientId::toString
-                    );
-                });
-        }
-        return Uni.createFrom().item(token);
-    }
+    // ...
 }
